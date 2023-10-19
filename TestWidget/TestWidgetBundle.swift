8c8291d5f01d304f120cb657caf022d9b9804776
Piggy(박진섭) <piggy.park@groundx.xyz>
2023-10-19 15:37:59 +0900

    add widget for test

---
+++ TestWidget/TestWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TestWidgetBundle.swift
+//  TestWidget
+//
+//  Created by gx_piggy on 10/18/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TestWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TestWidget()
+    }
+}
